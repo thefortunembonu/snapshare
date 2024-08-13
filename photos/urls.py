b88aa3d80a1209bb6e27ee2ea08b0c759f441d2b
fortune forrena <fortunembonu99@gmail.com>
2024-08-13 16:03:34 +0100

    new backend updates

--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ListCreateView, RetrieveUpdateView
+from .views import ListCreateView,CommentListCreateView, RetrieveUpdateView,CommentRetrieveUpdateView
 
 urlpatterns = [
     path('', ListCreateView.as_view()),
     path('<int:id>', RetrieveUpdateView.as_view()),
+    path('comments', CommentListCreateView.as_view()),
+    path('comment/<int:id>', CommentRetrieveUpdateView.as_view()),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions, generics
-from .models import Feed
-from .serializers import FeedSerializer
-from rest_framework.exceptions import PermissionDenied
+from .models import Feed, Comment
+from .serializers import FeedSerializer, CommentSerializer
+from rest_framework.exceptions import PermissionDenied,  NotFound
 
 class ListCreateView(generics.ListCreateAPIView):
     queryset = Feed.objects.all()
@@ -27,7 +27,37 @@ class RetrieveUpdateView(generics.RetrieveUpdateDestroyAPIView):
             raise PermissionDenied("You do not have permission to delete this object.")
   
     
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        feed_id = self.request.data.get('feed_id')  # Assuming 'feed_id' is sent in the POST data
+        try:
+            feed = Feed.objects.get(id=feed_id)
+        except Feed.DoesNotExist:
+            raise NotFound("Feed not found")
 
+        user = self.request.user
+        serializer.save(user=user, feed=feed)
+
+class CommentRetrieveUpdateView(generics.RetrieveUpdateDestroyAPIView):
+     queryset = Comment.objects.all()
+     serializer_class = CommentSerializer
+     permission_classes = [permissions.IsAuthenticated]
+     lookup_field = "id"
+     
+     def perform_destroy(self, instance):
+         user = self.request.user
+         feed_owner = instance.feed.user  
+        
+         if user.is_authenticated and (instance.user == user or feed_owner == user):
+            return super().perform_destroy(instance)
+         else:
+            raise PermissionDenied("You do not have permission to delete this object.")
+  
+    
      
 
 
